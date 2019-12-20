1d0458743f4a525b56c4757658255fdc67022594
Qiyue Zou <QIYUE.ZOU@pb.com>
2019-12-20 11:38:40 -0500

    modified Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -2,7 +2,7 @@ import java.lang.*;
 public class Planet{
         public double xxPos, yyPos, xxVel, yyVel, mass;
         public String imgFileName;
-        static final double G = 6.67e-11;
+        private static final double G = 6.67e-11;
         public Planet(double xP, double yP, double xV, double yV, double m, String img){
             this.xxPos = xP;
             this.yyPos = yP;
